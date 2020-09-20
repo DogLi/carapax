37287483464b995597830266081eb25d80d547fd
rossnomann <rossnomann@users.noreply.github.com>
2020-09-20 17:42:14 +0000

    Automated deployment: Sun Sep 20 17:42:13 UTC 2020 e259a3a406d428ce823c630d9a3512d05624f1fc

---
+++ implementors/snafu/trait.ErrorCompat.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["seance"] = [{"text":"impl ErrorCompat for FilesystemError","synthetic":false,"types":[]},{"text":"impl ErrorCompat for RedisError","synthetic":false,"types":[]}];
+implementors["snafu"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
